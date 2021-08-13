eb2f975c040eb1ccc8a66a5155835a89635e4512
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-08-13 09:50:45 +0700

    setupp

--- script/refresh.py
+++ script/refresh.py
@@ -16,7 +16,7 @@ os.system('flutter pub get')
 # build apk
 os.system('flutter packages pub run build_runner clean')
 os.system('flutter packages pub run build_runner build --delete-conflicting-outputs')
-os.system('flutter build apk --debug --flavor development --target lib/main_development.dart')
-# os.system('flutter build apk --release --flavor production --target lib/main_production.dart --target-platform android-arm64 --analyze-size')
+os.system('flutter build apk --debug --flavor development')
+# os.system('flutter build apk --release --flavor production --target-platform android-arm64 --analyze-size')
 
 print('refreshed')
